3cc3365d013c1f758162b98ca445e503e214c336
Weje Emmanuel <wejeemmanuel34@gmail.com>
2020-04-03 14:06:45 +0100

    fix

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -12,9 +12,6 @@
 
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 
-        <link rel="icon" href="/docs/4.4/assets/img/favicons/favicon.ico">
-
-
         <style>
         .bd-placeholder-img {
             font-size: 1.125rem;
